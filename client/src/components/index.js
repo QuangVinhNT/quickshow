fcb3759d376c8ba78a953cb746f927ffac03fb9c
ntqvinh <ntqvinh.251103@gmail.com>
2026-02-27 09:17:57 +0700

    feat(movie-detail): create movie detail page UI

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -2,7 +2,19 @@ import Footer from "./Footer";
 import NavBar from "./NavBar";
 import HeroSection from "./HeroSection";
 import FeatureSection from "./FeatureSection";
-import TrailersSection from './TrailersSection';
-import MovieCard from './MovieCard';
-import BlurCircle from './BlurCircle'
-export { Footer, NavBar, HeroSection, FeatureSection, TrailersSection, MovieCard, BlurCircle };
+import TrailersSection from "./TrailersSection";
+import MovieCard from "./MovieCard";
+import BlurCircle from "./BlurCircle";
+import DateSelect from "./DateSelect";
+import Loading from "./Loading";
+export {
+	Footer,
+	NavBar,
+	HeroSection,
+	FeatureSection,
+	TrailersSection,
+	MovieCard,
+	BlurCircle,
+	DateSelect,
+  Loading
+};
