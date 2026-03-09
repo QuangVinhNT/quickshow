d4209d2c4ea187adfb9ce11e6438901fcb1cd0b5
ntqvinh <ntqvinh.251103@gmail.com>
2026-03-09 08:42:55 +0700

    feat(models): create Movie model and Show model

---
+++ server/models/Show.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const showSchema = new mongoose.Schema(
+	{
+		movie: { type: String, require: true, ref: "Movie" },
+		showDateTime: { type: Date, requrire: true },
+		showPrice: { type: Number, require: true },
+		occupiedSeats: { type: Object, default: {} },
+	},
+	{
+		minimize: false,
+	},
+);
+
+const Show = mongoose.model("Show", showSchema);
+
+export default Show;
